9fd612e4b3da36c6487295c065dda23bfc706131
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-02-01 22:20:56 +0530

    logout bug fix: CORS bug

--- expo-app/api/api.ts
+++ expo-app/api/api.ts
@@ -1,7 +1,7 @@
 const API_URL = process.env.API_URL;
 
 export const fetchAuthUser = async () => {
-    const response = await fetch(`${API_URL}/auth/getHome`);
+    const response = await fetch(`${API_URL}/v1/auth/getHome`);
     const data = await response.json();
     if (data.error) return null;
     if (!response.ok) throw new Error(data.message || "Something went wrong");
